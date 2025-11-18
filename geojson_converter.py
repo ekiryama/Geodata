2c228f6f703da8996d2cb503bf28bc14999c11e1
ekiryama <35138463+ekiryama@users.noreply.github.com>
2025-11-18 09:13:19 +0300

    ICE-COT geojson_converter.py
    Includes ICE-COT Conversion below the original code.

--- geojson_converter.py
+++ geojson_converter.py
@@ -96,6 +96,70 @@ if uploaded_file is not None:
         )
         # print("GeoJSON file has been created successfully.")
 
-## ICE-COT conversion-------------------------------
+## ------------------------------  ICE-COT conversion  -------------------------------
+st.subheader("Upload the CSV (ICE-COT)")
+uploaded_file2 = st.file_uploader("Choose a csv file", type="csv")
 
+
+if uploaded_file2 is not None:
+    
+    # Get the file name without extension
+    file_name = os.path.splitext(uploaded_file2.name)[0]
+    
+    df2= pd.read_csv(uploaded_file2)
+    
+    # Check if all required columns are present
+    required_columns2 = ["Geometry", "Commodity", "Area", "ProducerCountry", "Variety", "CTOFarmerID", "Aggregator_ID", "CTOPlotID","DateOfMapping"]
+    missing_columns2 = [col for col in required_columns2 if col not in df2.columns]
+
+    if missing_columns2:
+        st.error(f"The file is not in the correct format, please use the current export from the dashboard with columns labeled: {', '.join(missing_columns2)}")
+    else:
+        st.subheader("Data Preview")
+        st.write("Total Number of features: ", df2.shape[0])
+        st.write(df2.head())
+        features = []
+        # Fix WKT polygons
+        df2["Geometry"] = df2["Geometry"].apply(fix_wkt)
+    
+        # Loop through each row in the DataFrame to create GeoJSON features
+        for _, row in df2.iterrows():
+            # Convert the WKT geometry to a shapely object
+            geometry = wkt.loads(row['Geometry'])
+            
+            # Create a GeoJSON feature with properties and geometry
+            feature = geojson.Feature(
+                geometry=mapping(geometry),
+                properties = {
+                     "ProducerCountry": str(row.get("ProducerCountry", "Unknown")),
+                    "Commodity": str(row.get("Commodity", "Unknown")),
+                    "Variety": str(row.get("Variety", "Unknown")),
+                    "CTOFarmerID": str(row.get("CTOFarmerID", "Unknown")),
+                    "Aggregator_ID": str(row.get("Aggregator_ID", "Unknown")),
+                    "CTOPlotID": str(row.get("CTOPlotID", "Unknown")),
+                    "DateOfMapping": str(row.get("DateOfMapping", "Unknown")),
+                     "Area": float(row.get("Area", 0))
+                }
+            )
+            
+            # Append the feature to the list of features
+            features.append(feature)
+            
+        # Define the output filename
+        geojson_filename = f"{file_name}.geojson"
+
+    
+        # Create a GeoJSON FeatureCollection
+        feature_collection = geojson.FeatureCollection(features)
+    
+        # # Save the FeatureCollection to a GeoJSON file
+        # with open('MyOutput.geojson', 'w') as f:
+        #     geojson.dump(feature_collection, f)
+    
+        st.download_button(
+            label="Download GeoJSON",
+            data= geojson.dumps(feature_collection),
+            file_name= geojson_filename,
+            mime="application/geo+json"
+        )
 
